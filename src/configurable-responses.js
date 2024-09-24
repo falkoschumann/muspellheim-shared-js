a9dad331c5a52ceb3bab6ef8acfa79b3dbbea417
Falko Schumann <falko.schumann@muspellheim.de>
2024-09-24 20:59:26 +0200

    docs: describe contract

--- src/configurable-responses.js
+++ src/configurable-responses.js
@@ -1,4 +1,27 @@
+/**
+ * Handles returning a pre-configured responses.
+ *
+ * This is one of the nullability patterns from James Shore for
+ * [testing without mocks](https://www.jamesshore.com/v2/projects/nullables/testing-without-mocks#output-tracking).
+ *
+ * ```javascript
+ * function createFetchStub(responses) {
+ *   const configurableResponses = ConfigurableResponses.create(responses);
+ *   return async function () {
+ *     const response = configurableResponses.next();
+ *     return {
+ *       status: response.status,
+ *       json: async () => response.body,
+ *     };
+ *   };
+ * }
+ * ```
+ */
 export class ConfigurableResponses {
+  /**
+   * Creates a configurable responses instance from a single response or an
+   * array of responses with an optional response name.
+   */
   static create(
     /** @type {Object|Array} */ responses,
     /** @type {string} */ name,
@@ -17,6 +40,12 @@ export class ConfigurableResponses {
     this.#responses = Array.isArray(responses) ? [...responses] : responses;
   }
 
+  /**
+   * Returns the next response.
+   *
+   * If there are no more responses, an error is thrown. If a single response is
+   * configured, it is always returned.
+   */
   next() {
     const response = Array.isArray(this.#responses)
       ? this.#responses.shift()
--- src/output-tracker.js
+++ src/output-tracker.js
@@ -1,4 +1,34 @@
+/**
+ * Tracks output events.
+ *
+ * This is one of the nullability patterns from James Shore for
+ * [testing without mocks](https://www.jamesshore.com/v2/projects/nullables/testing-without-mocks#output-tracking).
+ *
+ * Example implementation:
+ *
+ * ```javascript
+ * async record(event) {
+ *   // ...
+ *   this.dispatchEvent(new CustomEvent(EVENT_RECORDED_EVENT, { detail: event }));
+ * }
+ *
+ * trackEventsRecorded() {
+ *   return new OutputTracker(this, EVENT_RECORDED_EVENT);
+ * }
+ * ```
+ *
+ * Example usage:
+ *
+ * ```javascript
+ * const eventsRecorded = eventStore.trackEventsRecorded();
+ * // ...
+ * const data = eventsRecorded.data(); // [event1, event2, ...]
+ * ```
+ */
 export class OutputTracker {
+  /**
+   * Creates a tracker for a specific event of an event target.
+   */
   static create(
     /** @type {EventTarget} */ eventTarget,
     /** @type {string} */ event,
@@ -22,16 +52,25 @@ export class OutputTracker {
     this.#eventTarget.addEventListener(this.#event, this.#tracker);
   }
 
+  /**
+   * Returns the tracked data.
+   */
   get data() {
     return this.#data;
   }
 
+  /**
+   * Clears the tracked data and returns the cleared data.
+   */
   clear() {
     const result = [...this.#data];
     this.#data.length = 0;
     return result;
   }
 
+  /**
+   * Stops tracking.
+   */
   stop() {
     this.#eventTarget.removeEventListener(this.#event, this.#tracker);
   }
