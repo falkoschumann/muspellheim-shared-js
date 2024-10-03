5884c733337fc23bcbcbca103c9e657853967943
Falko Schumann <falko.schumann@muspellheim.de>
2024-10-03 09:42:43 +0200

    docs: improve contracts and test naming

--- test/store.test.js
+++ test/store.test.js
@@ -4,78 +4,84 @@ import { createStore } from '../lib/store.js';
 
 describe('Store', () => {
   describe('Create store', () => {
-    it('Creates store with initial state', () => {
+    it('returns a store with initial state', () => {
       const store = new createStore(reducer, initialState);
 
       expect(store.getState()).toEqual(initialState);
     });
 
-    it('Creates store and initializes state with reducer', () => {
+    it('initializes state with reducer', () => {
       const store = new createStore(reducer);
 
       expect(store.getState()).toEqual(initialState);
     });
   });
 
-  describe('Subscribe', () => {
-    it('Does not emit event, if state is not changed', () => {
+  describe('Dispatch', () => {
+    it('updates the state', () => {
       const { store } = configure();
-      let calls = 0;
-      const listener = () => calls++;
-      store.subscribe(listener);
-
-      store.dispatch({ type: 'unknown-action' });
-
-      expect(store.getState()).toEqual({ user: 'Alice' });
-      expect(calls).toBe(0);
-    });
-
-    it('Emits event, if state is changed', () => {
-      const { store } = configure();
-      let calls = 0;
-      const listener = () => calls++;
-      store.subscribe(listener);
 
       store.dispatch({ type: 'user-changed', name: 'Bob' });
 
       expect(store.getState()).toEqual({ user: 'Bob' });
-      expect(calls).toBe(1);
     });
 
-    it('Does not emit event, if listener is unsubscribed', () => {
+    it('does not update the state when the action is unknown', () => {
       const { store } = configure();
-      let calls = 0;
-      const listener = () => calls++;
+
+      store.dispatch({ type: 'unknown-action' });
+
+      expect(store.getState()).toEqual(preloadedState);
+    });
+  });
+
+  describe('Subscribe', () => {
+    it('notifies the listener when the state is changed', () => {
+      const { store, listener } = configure();
+      store.subscribe(listener);
+
+      store.dispatch({ type: 'user-changed', name: 'Bob' });
+
+      expect(listener.getCalledTimes()).toBe(1);
+    });
+
+    it('does not notify the listener when the state is not changed', () => {
+      const { store, listener } = configure();
+      store.subscribe(listener);
+
+      store.dispatch({ type: 'unknown-action' });
+
+      expect(listener.getCalledTimes()).toBe(0);
+    });
+
+    it('does not notify the listener when the listener is unsubscribed', () => {
+      const { store, listener } = configure();
       const unsubscribe = store.subscribe(listener);
 
       unsubscribe();
       store.dispatch({ type: 'user-changed', name: 'Bob' });
 
-      expect(store.getState()).toEqual({ user: 'Bob' });
-      expect(calls).toBe(0);
+      expect(listener.getCalledTimes()).toBe(0);
     });
 
-    it('Ignores unsubscribed listener', () => {
+    it('ignores an unsubscribed listener', () => {
       const { store } = configure();
-      let calls = 0;
       let unsubscribe2;
-      const listener1 = () => {
-        calls++;
-        unsubscribe2();
-      };
-      const listener2 = () => calls++;
+      const listener1 = createListener(() => unsubscribe2());
+      const listener2 = createListener();
       store.subscribe(listener1);
       unsubscribe2 = store.subscribe(listener2);
 
       store.dispatch({ type: 'user-changed', name: 'Bob' });
 
       expect(store.getState()).toEqual({ user: 'Bob' });
-      expect(calls).toBe(1);
+      expect(listener2.getCalledTimes()).toBe(0);
     });
   });
 });
 
 const initialState = { user: '' };
+const preloadedState = { user: 'Alice' };
 
 function reducer(state = initialState, action) {
   switch (action.type) {
@@ -87,6 +93,17 @@ function reducer(state = initialState, action) {
 }
 
 function configure() {
-  const store = new createStore(reducer, { user: 'Alice' });
-  return { store };
+  const store = new createStore(reducer, preloadedState);
+  const listener = createListener();
+  return { store, listener };
+}
+
+function createListener(action = () => {}) {
+  let calls = 0;
+  const listener = () => {
+    calls++;
+    action();
+  };
+  listener.getCalledTimes = () => calls;
+  return listener;
 }
